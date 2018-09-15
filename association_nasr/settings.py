df0ebf42443cb19df709120ebb6e182b647de84d
ismailkasmi <ismailkasmi.here@gmail.com>
2018-09-15 15:14:39 +0200

    Add STATIC_ROOT configuration

--- association_nasr/settings.py
+++ association_nasr/settings.py
@@ -127,10 +127,10 @@ STATICFILES_DIRS = [
     #'/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "src/staticfiles")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "src/media_cdn")
 
 
 
